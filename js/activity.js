e3bda16db49d035df3fec2d9f110aba1a8ebcf65
Manuel Quiñones <manuq@laptop.org>
2013-05-23 14:07:36 -0300

    Initialize with random colors

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,7 @@
 define(function (require) {
     var activity = require("sugar-html-activity/activity");
     var icon = require("sugar-html-graphics/icon");
+    var xocolor = require("activity/xocolor");
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
@@ -10,10 +11,10 @@ define(function (require) {
 
         // Colorize the activity icon.
         var activityButton = document.getElementById("activity-button");
-        var iconCenter = document.querySelector("#icon-center");
+        var iconCurrent = document.getElementById("icon-current");
         activity.getXOColor(function (colors) {
             icon.colorize(activityButton, colors);
-            icon.colorize(iconCenter, colors);
+            icon.colorize(iconCurrent, colors);
         });
 
         // Make the activity stop with the stop button.
@@ -21,6 +22,15 @@ define(function (require) {
         stopButton.onclick = function () {
             activity.close();
         };
+
+        var colorButtons = document.querySelectorAll("#color-icons button");
+        console.log(colorButtons);
+        for (i = 0; i < colorButtons.length; i++) {
+            button = colorButtons[i];
+            var randomColors = xocolor.colors[Math.floor(Math.random() *
+                xocolor.colors.length)];
+            icon.colorize(button, randomColors);
+        }
 
     });
 
